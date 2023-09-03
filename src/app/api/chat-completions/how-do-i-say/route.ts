3e0a92d2e625e072c9e164e208212c2f7e03cc64
halftone <86031051+halftonenano@users.noreply.github.com>
2023-09-03 03:10:41 -0700

    fixed ai misinterpreting on /how-do-i-say

--- src/app/api/chat-completions/how-do-i-say/route.ts
+++ src/app/api/chat-completions/how-do-i-say/route.ts
@@ -15,6 +15,11 @@ export async function POST(request: NextRequest) {
   if (!language)
     return new Response('language is not selected', { status: 400 });
 
+  messages[0] = {
+    role: 'user',
+    content: `How do I say "${messages[0].content}" in ${language}?`,
+  };
+
   messages.unshift({
     role: 'system',
     content: `Walk the user through how to say the following sentence in ${language}. Do not out right tell them the answer but instead go step by step showing them the words they may need. DO NOT TELL THEM THE FULL SENTENCE!`,
